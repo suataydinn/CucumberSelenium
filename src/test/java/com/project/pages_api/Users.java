b7d43f15366dad0584141bafcc02a54d7886ddc0
suat525255 <suataydin5255@gmail.com>
2024-12-12 09:58:23 +0300

    added users private class

---
+++ src/test/java/com/project/pages_api/Users.java
@@ -0,0 +1,29 @@
+package com.project.pages_api;
+
+public class Users {
+
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+
+    // Getters
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+
+
+}
